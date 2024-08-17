0cd50d679d3e8435577c7900e07a18330969c32d
augustodevcode <40444250+augustodevcode@users.noreply.github.com>
2024-08-17 15:38:26 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main template file
+ * 
+ * @package bidexpert
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Bid Expert Theme</title>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
